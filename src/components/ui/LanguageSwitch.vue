a128ea4a0b32427f78e33961c0451f11c93b4909
León Stagliano <stagliano.leonardo@gmail.com>
2026-08-11 20:06:00 +0200

    New components for navbar actions

---
+++ src/components/ui/LanguageSwitch.vue
@@ -0,0 +1,55 @@
+<template>
+    <button class="lang-switch" :aria-label="$t('nav.toggleLang')" @click="toggleLang">
+        <div class="language" :class="{ 'selected': currentLang === 'es' }">ES</div>
+        <div class="language" :class="{ 'selected': currentLang === 'en' }">EN</div>
+    </button>
+</template>
+
+
+<!-- <button class="navbar__lang-toggle" :aria-label="$t('nav.toggleLang')" @click="toggleLang">
+    <LanguageIcon :current-lang="currentLang" />
+</button> -->
+
+<script setup>
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+// import LanguageIcon from '@/components/icons/LanguageIcon.vue'
+
+const { locale } = useI18n()
+
+const currentLang = ref(locale.value)
+
+const toggleLang = () => {
+    locale.value = locale.value === 'es' ? 'en' : 'es'
+    currentLang.value = locale.value
+}
+</script>
+
+<style scoped>
+.lang-switch {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 30px;
+    background: none;
+    border: none;
+    border-radius: .5rem;
+    cursor: pointer
+}
+
+.language {
+    border: 1px solid var(--disabled-color);
+    padding: 4px;
+    color: var(--disabled-color);
+    font-family: var(--font-hud);
+    font-size: 0.875rem;
+    font-weight: 500;
+    letter-spacing: -0.01rem;
+}
+
+.selected{
+    color: var(--main-color);
+    border: 1px solid var(--main-color);
+    font-weight: 700;
+}
+</style>
---
+++ src/components/ui/ToggleThemeSwitch.vue
@@ -0,0 +1,56 @@
+<template>
+    <button :aria-label="$t('nav.toggleTheme')" @click="toggleTheme" class="switch" :class="{ 'is-dark': isDark }"
+        aria-label="THEME_TOGGLE">
+        <LightModeIcon class="icon sun-icon" />
+        <DarkModeIcon class="icon moon-icon" />
+    </button>
+</template>
+
+<script setup>
+import { useTheme } from '@/composables/useTheme'
+import LightModeIcon from '../icons/LightModeIcon.vue';
+import DarkModeIcon from '../icons/DarkModeIcon.vue';
+
+const { isDark, toggleTheme } = useTheme()
+</script>
+
+<style scoped>
+.switch {
+    display: flex;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+    border: none;
+    background: none;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.2s ease-in-out;
+}
+
+.icon {
+    position: absolute;
+    height: 20px;
+    width: 20px;
+    transition: transform 0.2s ease, opacity 0.15s;
+}
+
+.sun-icon {
+    opacity: 1;
+    transform: scale(1);
+}
+
+.switch.is-dark .sun-icon {
+    opacity: 0;
+    transform: scale(0.5) cubic-bezier(0.6, -0.28, 0.735, 0.045);
+}
+
+.moon-icon {
+    opacity: 0;
+    transform: scale(0.5);
+}
+
+.switch.is-dark .moon-icon {
+    opacity: 1;
+    transform: scale(1);
+}
+</style>
