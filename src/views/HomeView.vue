339eb159700a67ce64456179894a95b4bc9aa862
León Stagliano <stagliano.leonardo@gmail.com>
2026-08-13 20:00:07 +0200

    New sections included. 'AboutMe' and 'Methodology'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,49 +1,35 @@
 <script setup>
 import Navbar from '@/components/layout/Navbar.vue'
 import HeroSection from '@/components/sections/HeroSection.vue'
-// import AboutMe from '@/components/sections/AboutMe.vue'
+import AboutMe from '@/components/sections/AboutMe.vue'
 // import Skills from '@/components/sections/Skills.vue'
 // import Projects from '@/components/sections/Projects.vue'
 // import Methodology from '@/components/sections/Methodology.vue'
 // import Experience from '@/components/sections/Experience.vue'
 // import Education from '@/components/sections/Education.vue'
 // import Contact from '@/components/sections/Contact.vue'
+import Footer from '@/components/layout/Footer.vue'
 </script>
 
 <template>
-  <div id="home">
+  <!-- <div id="home"> -->
+  <main class="home__content">
     <Navbar />
     <HeroSection />
-    <main class="home__content">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eius doloribus, in placeat, recusandae, quis sit possimus amet sequi nulla consequatur quibusdam doloremque odio? Temporibus libero ipsam distinctio doloremque doloribus!
-      Neque unde magnam consectetur commodi officia consequatur! Repudiandae vero nobis, tempora, expedita quo sed facilis autem nostrum veniam facere praesentium iure quam consectetur consequatur error ducimus alias molestias! In, omnis?
-      Impedit quae id repellendus adipisci inventore, omnis quam, voluptate blanditiis aliquid rem, quod praesentium unde esse aut nesciunt corporis perferendis accusantium dignissimos natus obcaecati facere! Possimus fugit quasi enim illo.
-      Atque commodi veritatis molestias quidem magnam reiciendis minus est voluptates asperiores eos, aspernatur explicabo quos cum tenetur quaerat distinctio fuga eaque itaque maxime totam facere officia? Quis soluta id voluptas?
-      Vel odio velit natus optio ducimus, obcaecati aliquam quod reprehenderit aperiam, molestias perspiciatis deserunt! Esse voluptatem ut, cumque modi repellendus accusamus saepe alias repudiandae consectetur doloremque consequatur, iure inventore sed.
-      Neque optio deleniti, natus alias amet eaque sapiente ullam dignissimos culpa est molestiae quidem consequuntur maxime sunt, itaque voluptas voluptatem debitis omnis quis iusto harum. Modi reiciendis ducimus tempore harum.
-      Odit corporis et culpa ducimus non inventore quos quia obcaecati officiis unde praesentium, rem doloremque magni? Ut vero, quisquam fugiat pariatur ex esse veniam doloribus repellat quis. Consequuntur, quidem neque?
-      Blanditiis, distinctio corrupti culpa cumque unde quisquam, consectetur laborum magni dolore voluptas nostrum! Iure, error repellendus? Corrupti debitis odio rerum esse ad tempore cumque. Hic voluptatem facere id explicabo adipisci?
-      Sapiente aut et voluptatum, similique quod aliquid, voluptas est, rerum esse minima vitae! Sapiente, voluptates ut. Suscipit explicabo ipsum nesciunt repellat eos laborum soluta ducimus dolorem a optio? Expedita, qui.
-      Perspiciatis excepturi illum fugit aliquid dolorum sint amet, recusandae autem labore ipsum, ut hic quam nesciunt dolorem cumque molestiae deserunt accusantium. Mollitia, magni? Beatae earum incidunt doloremque ad fugiat maiores.
-      Incidunt saepe facilis cumque neque eos, consectetur numquam pariatur dolore inventore minus ad modi ratione eveniet eaque praesentium, illum aut unde nihil a. Maxime, perspiciatis! Placeat voluptatum quas earum itaque.
-      Culpa saepe ex minima repudiandae! Minus, iusto excepturi delectus praesentium nostrum vero ullam, in libero incidunt consequuntur omnis, accusantium quidem repellendus eaque doloribus neque atque tempora nobis illo sed officia!
-      Facilis quo consectetur dignissimos aspernatur alias sit sapiente tempora iste natus consequatur maxime excepturi, quidem perspiciatis nihil, eius cumque iure distinctio. Sint temporibus accusamus excepturi voluptatibus facilis accusantium non aperiam.
-      Dignissimos ullam autem minus! Consequatur eaque neque voluptatibus illo voluptatum quam, saepe assumenda placeat laboriosam reprehenderit? Dolorem repudiandae nulla suscipit commodi exercitationem quisquam eius adipisci in id, culpa nemo repellat!
-      Eaque ullam repellendus neque assumenda praesentium, unde laborum dolores perferendis, culpa quo eligendi distinctio odio amet, cum ea aspernatur beatae quisquam est omnis voluptate commodi aliquid laudantium. Iusto, maxime hic.
-      Dicta nostrum laboriosam voluptas aliquid qui? Tempora dolore, quia cumque nisi illum eaque eius quisquam inventore quod non voluptatibus fugiat unde harum est saepe. Dolores pariatur cum possimus hic aspernatur.
-      <AboutMe />
-      <Skills />
-      <Projects />
-      <Methodology />
-      <Experience />
-      <Education />
-      <Contact />
-    </main>
-  </div>
+    <AboutMe />
+    <Skills />
+    <Projects />
+    <Methodology />
+    <Experience />
+    <Education />
+    <Contact />
+    <Footer />
+  </main>
+  <!-- </div> -->
 </template>
 
 <style scoped>
-  #home {
+.home_content {
   width: 100%;
   min-height: 100vh;
   background: var(--bg-primary);
